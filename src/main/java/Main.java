c1f5058207062ec591b7c5fee9a0c2d11f0ad984
Cezary Broś <cezary.bros+git@gmail.com>
2017-05-04 02:05:44 +0200

    Add path to details

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import controller.EventController;
+import dao.DatabaseConnect;
+import dao.EventDaoSqlite;
+import model.Event;
 import spark.Request;
 import spark.Response;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.Date;
+import java.util.List;
 
 import  static spark.Spark.*;
 
@@ -13,19 +15,22 @@ import  static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(new Date());
         exception(Exception.class, (e, req, res) -> e.printStackTrace());
-        staticFileLocation("/public");
+        staticFileLocation("/static");
         port(8888);
-
-        // Always start with more specific routes
-        get("/hello", (req, res) -> "Hello World");
-
+        DatabaseConnect.getInstance();
         // Always add generic routes to the end
         get("/", EventController::renderProducts, new ThymeleafTemplateEngine());
         // Equivalent with above
         get("/index", (Request req, Response res) -> {
             return new ThymeleafTemplateEngine().render( EventController.renderProducts(req, res) );
         });
+        get("/event/:id", (Request req, Response res) -> {
+            return new ThymeleafTemplateEngine().render(
+                    EventController.eventDetails(req, res, Integer.parseInt(req.params(":id"))) );
+        });
+
     }
 
 
