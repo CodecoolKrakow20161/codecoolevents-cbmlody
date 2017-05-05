b460bc134b7fef546dcbbe5215f638a4bfad9b53
Cezary Broś <cezary.bros+git@gmail.com>
2017-05-06 00:28:02 +0200

    Refactor EventDao interface

--- src/main/java/dao/EventDao.java
+++ src/main/java/dao/EventDao.java
@@ -1,16 +1,16 @@
 package dao;
 
+import model.Category;
 import model.Event;
 
-import java.util.Date;
 import java.util.List;
 
 
 public interface EventDao {
-    void add();
-    void remove(int id);
+    void add(Event event);
+    boolean remove(int id);
     void edit();
     List<Event> getAll();
-    List<Event> getBy(Date date);
+    List<Event> getBy(Category category);
 
 }
