50d4b00f44b80ed18084816726b969706ac83629
Cezary Broś <cezary.bros+git@gmail.com>
2017-05-06 00:27:25 +0200

    Implement find methods, Implement execute Query

--- src/main/java/dao/CategoryDaoSqlite.java
+++ src/main/java/dao/CategoryDaoSqlite.java
@@ -25,7 +25,31 @@ public class CategoryDaoSqlite implements CategoryDao {
         return categoryList;
     }
 
+    @Override
+    public Category find(String name) {
+        String query = "SELECT * FROM `categories` WHERE name ='" + name + "'";
+        return executeQuery(query);
+    }
+
+    @Override
+    public Category find(int id) {
+        String query = "SELECT * FROM `categories` WHERE id='" + id + "'";
+        return executeQuery(query);
+    }
+
     private Category categoryResultSet(ResultSet resultSet) throws SQLException{
         return new Category( resultSet.getInt("id"), resultSet.getString("name") );
+    }
+
+    private Category executeQuery (String query) {
+        Category category = null;
+        Statement statement = DatabaseConnect.getInstance().getStatement();
+        try {
+            ResultSet resultSet = statement.executeQuery(query);
+            return categoryResultSet(resultSet);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return category;
     }
 }
