ef0ce9b6321299ec3d105e06a949e4bb1531f978
Cezary Broś <cezary.bros+git@gmail.com>
2017-05-07 03:18:51 +0200

    Code cleanup, Add  missing throw exception

--- src/main/java/dao/CategoryDaoSqlite.java
+++ src/main/java/dao/CategoryDaoSqlite.java
@@ -22,18 +22,18 @@ public class CategoryDaoSqlite implements CategoryDao {
     }
 
     @Override
-    public Category find(String name) {
-        String query = "SELECT * FROM `categories` WHERE name ='" + name + "'";
+    public Category find(String name) throws SQLException {
+        String query = "SELECT * FROM `categories` WHERE name ='" + name + "';";
         return executeQuery(query);
     }
 
     @Override
-    public Category find(int id) {
-        String query = "SELECT * FROM `categories` WHERE id='" + id + "'";
+    public Category find(int id) throws SQLException {
+        String query = "SELECT * FROM `categories` WHERE id='" + id + "';";
         return executeQuery(query);
     }
 
-    private Category categoryResultSet(ResultSet resultSet) throws SQLException{
+    private Category categoryResultSet(ResultSet resultSet) throws SQLException {
         return new Category( resultSet.getInt("id"), resultSet.getString("name") );
     }
 
